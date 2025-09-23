291ec8b95a76caec33f696fa63f5e0af140fefdc
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2025-09-23 14:58:36 +0600

    9-password-strength done

--- Slices/8-message-filter/main.go
+++ Slices/8-message-filter/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Message interface {
 	Type() string
 }
@@ -36,5 +38,51 @@ func (lm LinkMessage) Type() string {
 // Don't touch above this line
 
 func filterMessages(messages []Message, filterType string) []Message {
-	// ?
+	filteredMsg := []Message{} // empty slice of type Message struct
+
+	for _, message := range messages {
+		if message.Type() == filterType {
+			filteredMsg = append(filteredMsg, message)
+		}
+	}
+
+	return filteredMsg
+}
+
+func main() {
+	msgs := []Message{
+		TextMessage{
+			Sender:  "Text1",
+			Content: "Content1",
+		},
+		MediaMessage{
+			Sender:    "Text4",
+			MediaType: "MediaType2",
+			Content:   "Content4",
+		},
+		LinkMessage{
+			Sender:  "Text6",
+			URL:     "URL2",
+			Content: "Content6",
+		},
+		TextMessage{
+			Sender:  "Text2",
+			Content: "Content2",
+		},
+		MediaMessage{
+			Sender:    "Text3",
+			MediaType: "MediaType1",
+			Content:   "Content3",
+		},
+		LinkMessage{
+			Sender:  "Text5",
+			URL:     "URL1",
+			Content: "Content5",
+		},
+	}
+
+	filteredMsgs := filterMessages(msgs, "link")
+	fmt.Println(filteredMsgs)
+
+	return
 }
---
+++ Slices/9-password-strength/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func isValidPassword(password string) bool {
+	if len(password) <= 5 && len(password) >= 12 {
+		return false
+	}
+
+	containsUppercase := false
+	containsNum := false
+	for _, char := range password {
+		// Contains at least one uppercase letter.
+		if char >= 65 && char <= 90 {
+			containsUppercase = true
+		}
+		if char >= 48 && char <= 57 {
+			containsNum = true
+		}
+		// Contains at least one digit.
+	}
+
+	return containsUppercase && containsNum
+}
+
+func main() {
+	res := isValidPassword("Badmove123")
+	fmt.Println(res)
+}
