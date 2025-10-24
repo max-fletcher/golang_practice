e1eb99f1e6077ea625fb4434b2a474d4f0a6bcb6
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2025-10-24 17:58:55 +0600

    Concurrency channels(L3) done

--- Concurrency/3-channels(L3)/main.go
+++ Concurrency/3-channels(L3)/main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func waitForDBs(numDBs int, dbChan chan struct{}) {
-	<-dbChan
+	for i := 0; i < numDBs; i++ {
+		<-dbChan
+		fmt.Printf("Received message from database %v\n", i+1)
+	}
 }
 
 // don't touch below this line
@@ -24,5 +27,12 @@ func getDBsChannel(numDBs int) (chan struct{}, *int) {
 }
 
 func main() {
+	count := 5
+	ch, _ := getDBsChannel(count)
+	waitForDBs(count, ch)
 
+	// If you use these 2 lines, the main thread(and waitForDBs) might finish executing when count == 0 so it might not run properly(i.e Will run 0 times and not 5 times)
+	// and might not show the fmts properly
+	// ch, count := getDBsChannel(3)
+	// waitForDBs(count, ch)
 }
